4fdb3d5b38c0309db3c632dd926c4615876553fd
wifqo <akromasyams11@gmail.com>
2025-02-22 16:01:19 +0700

    update/printing preview

--- lib/widgets/stateful/printing.dart
+++ lib/widgets/stateful/printing.dart
@@ -52,41 +52,98 @@ class _PrintingState extends State<Printing> {
     return pdf.save();
   }
 
-  pw.Column receipt() {
-    return pw.Column(
-      children: [
-        pw.Center(
-          child: pw.Text(
-            "Bukti Pembayaran Zakat",
+  pw.Container receipt() {
+    late String totalRP = "0";
+    late String totalKg = "0";
+
+    for (var v in widget.muzakkiData) {
+      double currentAmount = double.parse(v.amount);
+
+      if (v.zakatType == ZakatType.uang) {
+        double currentTotalRP = double.parse(totalRP);
+        currentTotalRP += currentAmount;
+        totalRP = currentTotalRP.toStringAsFixed(2);
+      } else {
+        double currentTotalKg = double.parse(totalKg);
+        currentTotalKg += currentAmount;
+        totalKg = currentTotalKg.toString();
+      }
+    }
+
+    return pw.Container(
+      decoration: pw.BoxDecoration(
+        border: pw.Border.all(color: PdfColors.black),
+      ),
+      padding: pw.EdgeInsets.all(15),
+      child: pw.Column(
+        children: [
+          pw.Center(
+            child: pw.Text(
+              textAlign: pw.TextAlign.center,
+              "PANITIA (AMIL) ZAKAT FITRAH\nBKM MASJID MA'SHUM THO'AT RT 15 SIDOMULYO BENGKULU 1446 H/2025 M",
+            ),
           ),
-        ),
-        pw.Divider(height: 2.5, thickness: 2),
-        pw.Column(
-          children: List.generate(
-            widget.muzakkiData.length,
-            (index) {
-              MuzakkiInputData muzakkiData = widget.muzakkiData[index];
-              return pw.Row(
-                mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
-                children: [
-                  pw.Expanded(
-                    child: pw.Text(muzakkiData.name),
-                  ),
-                  pw.Expanded(
-                    child: pw.Text(muzakkiData.zakatType == ZakatType.uang
-                        ? 'Uang'
-                        : 'Beras'),
-                  ),
-                  pw.Expanded(
-                    child: pw.Text(
-                        "${muzakkiData.zakatType == ZakatType.uang ? 'Rp.' : ''} ${muzakkiData.amount} ${muzakkiData.zakatType == ZakatType.beras ? 'Kg' : ''}"),
-                  ),
-                ],
-              );
-            },
+          pw.SizedBox(
+            height: 10,
           ),
-        )
-      ],
+          pw.Text("MUZAKKI/ORANG PEMBAYAR ZAKAT:"),
+          pw.SizedBox(
+            height: 10,
+          ),
+          pw.Column(
+            children: List.generate(
+              widget.muzakkiData.length,
+              (index) {
+                MuzakkiInputData muzakkiData = widget.muzakkiData[index];
+                return pw.Row(
+                  mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                  children: [
+                    pw.Text("${index + 1}. "),
+                    pw.Expanded(
+                      child: pw.Text(muzakkiData.name),
+                    ),
+                    pw.Expanded(
+                      child: pw.Text(muzakkiData.zakatType == ZakatType.uang
+                          ? 'Uang'
+                          : 'Beras'),
+                    ),
+                    pw.Expanded(
+                      child: pw.Text(
+                          "${muzakkiData.zakatType == ZakatType.uang ? 'Rp.' : ''} ${muzakkiData.amount} ${muzakkiData.zakatType == ZakatType.beras ? 'Kg' : ''}"),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+          pw.SizedBox(
+            height: 15,
+          ),
+          pw.Row(
+            mainAxisAlignment: pw.MainAxisAlignment.spaceEvenly,
+            children: [
+              pw.Text("Total Rp: $totalRP"),
+              pw.Text("Total Kg: $totalKg"),
+            ],
+          ),
+          pw.Text("Lafadz Niat Menunaikan Zakat Fitrah:"),
+          pw.SizedBox(
+            height: 5,
+          ),
+          pw.Text(
+              textAlign: pw.TextAlign.center,
+              "Arab-Latin: Nawaytu an-ukhrija zakaatal-fitri 'an-ni wa 'an-jami'i ma yalzimuniy nafaqatuhum syar'an fardhan lillahi ta'ala"),
+          pw.SizedBox(
+            height: 5,
+          ),
+          pw.Text(
+              textAlign: pw.TextAlign.center,
+              "Artinya: Sengaja aku mengeluarkan zakat fitrah untuk diriku dan seluruh orang yang nafkahnya menjadi tanggunganku fardhu karena Allah ta'ala"),
+          pw.SizedBox(
+            height: 5,
+          ),
+        ],
+      ),
     );
   }
 }
